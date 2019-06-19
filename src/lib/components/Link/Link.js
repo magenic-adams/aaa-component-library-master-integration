e6827c3f5afae706860d6dbbc1475f8644206e64
Enzo Matela <EnzoM@Magenic.com>
2019-06-19 17:53:44 +0800

    Change code from withStlyes to makeStyles

--- src/lib/components/Link/Link.js
+++ src/lib/components/Link/Link.js
@@ -1,60 +1,84 @@
 import React from 'react';
-import { withStyles } from '@material-ui/styles'; // listed as a dependency
-import cx from 'clsx';
+import PropTypes from 'prop-types';
+import { withTheme, makeStyles } from '@material-ui/styles'; // listed as a dependency
 import { Link as MuiLink } from '@material-ui/core'; // used the Link instead of the button
 
 
-const styleClasses = theme => ({
-    primary : { 
-            color: theme.palette.colorVariables.DARKER_BLUE,
+const useStyles = makeStyles({
+    colorPrimary: {
+            color: props => props.theme.palette.colorVariables.DARKER_BLUE,
             cursor: 'pointer',
-            fontSize: 18,
+            fontSize: 16,
             fontWeight: 'normal',
             fontStyle: 'normal',
             fontStretch: 'normal',
             lineHeight: 2.67,
             letterSpacing: 'normal',
-            padding: '12px 0 12px 0',
-            [theme.breakpoints.down('md')]: {
-                fontSize: 16,
-                lineHeight: 3,
-                padding: '13.5px 0 13.5px 0',
-            },
-            '&:hover': {
-                color:theme.palette.colorVariables.VERY_DARK_BLUE,
-            }
-    },
-    secondary: {
-            color: theme.palette.colorVariables.DARKER_BLUE,
-            cursor: 'pointer',
-            fontWeight: 500,
-            fontSize: 16,
-            fontStyle: 'normal',
-            fontStretch: 'normal',
-            lineHeight: 2.75,
-            letterSpacing: 'normal',
             padding: '13.5px 0 13.5px 0',
-            [theme.breakpoints.down('md')]: {
-                fontSize: 14,
-                lineHeight: 3.14,
-                letterSpacing: 'normal',
-                padding: '14.5px 0 14.5px 0'
+            [props => props.theme.breakpoints.down('md')]: {
+                fontSize: 18,
+                lineHeight: 3,
+                padding: '12px 0 12px 0',
             },
             '&:hover': {
-                color:theme.palette.colorVariables.VERY_DARK_BLUE,
+                color: props => props.theme.palette.colorVariables.VERY_DARK_BLUE,
             },
+        },
+        colorSecondary: {
+                color: props => props.theme.palette.colorVariables.DARKER_BLUE,
+                cursor: 'pointer',
+                fontWeight: props => props.theme.typography.fontWeight,
+                fontSize: 16,
+                fontStyle: 'normal',
+                fontStretch: 'normal',
+                lineHeight: 2.75,
+                letterSpacing: 'normal',
+                padding: '13.5px 0 13.5px 0',
+                [props => props.theme.breakpoints.down('md')]: {
+                    fontSize: 14,
+                    lineHeight: 3.14,
+                    letterSpacing: 'normal',
+                    padding: '14.5px 0 14.5px 0',
+                },
+                '&:hover': {
+                    color: props => props.theme.palette.colorVariables.VERY_DARK_BLUE,
+                },
         }, 
-  })
+  });
 
-    const Link = ({ id, className, onClick, classes, href, children, onBlur }) =>
-        <MuiLink 
+  type propTypes = {
+    children: PropTypes.string,
+    color: PropTypes.bool,
+    id?: PropTypes.bool,
+    href?: PropTypes.bool,
+    onBlur: () => {},
+    onClick: () => {}
+  };
+
+    const Link = (props:propTypes) => {
+        const { children, color, id, href, onBlur, onClick } = props;
+        const classes = useStyles(props);
+        return (
+            <MuiLink 
+            color={color}
             id={id}
-            className={cx("Link", [className ==='primary' ? classes.primary : classes.secondary] )}
             href={href}
             onBlur={onBlur}
             onClick={onClick}
+            TypographyClasses={{
+                colorPrimary: classes.colorPrimary,
+                colorSecondary: classes.colorSecondary,
+            }}
         >
             {children}
         </MuiLink>
+        );
+    };
 
-export default withStyles(styleClasses, {withTheme: true})(Link);
+    Link.defaultProps = {
+        id: '',
+        href: '',
+      };
+
+
+export default withTheme(Link);
