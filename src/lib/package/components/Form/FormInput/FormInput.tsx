44bf07e770857538620f5fabdc3098a1fe30a158
Adam Schuld <elgrecode@gmail.com>
2019-06-20 11:54:04 -0700

    fixes FormInput into class component and builds HOC withFormState to inject form state

--- src/lib/package/components/Form/FormInput/FormInput.tsx
+++ src/lib/package/components/Form/FormInput/FormInput.tsx
@@ -1,57 +1,20 @@
-import React, { useRef } from 'react';
-import { Field, useForm } from 'react-final-form';
+import React from 'react';
+import { Field } from 'react-final-form';
 
 // Components
+import withFormState from '../withFormState';
 import BaseInput from '../../Input/BaseInput/BaseInput';
 
-type RequiredProps = {
+interface RequiredProps {
   id: string,
   name: string,
 };
 
-type OptionalProps = {
-  forwardedRef: {current: {focus: () => void}}
+interface OptionalProps {
+  formState?: any, // Decorator
+  forwardedRef?: React.RefObject<any>,
 };
 
-/**
- * Form field change interceptor calls a form effect "setFieldTouched"
- * @param  {Object} options.input - fieldProps.input
- * @param  {Object} formState - global form state
- * @return {Function} decoratored onChange
- */
-function handleFormFieldChange(
-  { input }:{input: {name: string, onChange: (evt:React.SyntheticEvent) => void}},
-  formState:any
- ):(evt:React.SyntheticEvent) => void {
-  return (evt) => {
-    const { name, onChange } = input;
-    const { mutators: { setFieldTouched }} = formState;
-    setFieldTouched(name, false);
-    onChange(evt);
-  };
-}
-
-
-/**
- * Handles clearing of input value
- * @param  {Object} options.input - fieldProps.input
- * @param  {Object} formState - global form state
- * @param  {Object} inputRef - reference to the underlying input
- * @return {Function} onClear handler
- */
-function handleFieldClear(
-  { input }:{input: {name: string, onChange: (val:string) => void}},
-  formState:any,
-  inputRef: {current: {focus: () => void}}
-  ){
-  return () => {
-    const { name, onChange } = input;
-    const { mutators: { setFieldTouched }} = formState;
-    setFieldTouched(name, false);
-    onChange('');
-    inputRef.current.focus();
-  };
-}
 
 /**
  * FormInput is a functional <Field> Wrapper around <BaseInput />
@@ -62,30 +25,86 @@ function handleFieldClear(
  * By exposing the form's state via hooks, we are able to tap into custom mutators and other form state
  * defined on our top-level <Form> component and plucked from context
  */
-const FormInput:React.FunctionComponent<RequiredProps & OptionalProps> = (props) => {
-  const { forwardedRef } = props;
-  const inputRef = forwardedRef || useRef(null);
-  const formState = useForm();
-  const { name } = props;
-  return (
-    <Field
-      name={name}
-      component={(fieldProps) => {
-        const { meta } = fieldProps;
-        return (
-          <BaseInput
-            {...fieldProps.input}
-            error={meta.touched && meta.error}
-            onChange={handleFormFieldChange(fieldProps, formState)}
-            onClear={handleFieldClear(fieldProps, formState, inputRef)}
-            forwardedRef={inputRef}
-            {...props} // Passed props take highest priority
-          />
-        );
-      }}
-    />
-  );
-};
+class FormInput extends React.Component<RequiredProps & OptionalProps> {
+  constructor(props:RequiredProps & OptionalProps){
+    super(props);
+    this.handleFormFieldChange = this.handleFormFieldChange.bind(this);
+    this.handleFieldClear = this.handleFieldClear.bind(this);
+    this.renderFieldComponent = this.renderFieldComponent.bind(this);
+  }
+
+  /**
+   * Form field change interceptor calls a form effect "setFieldTouched"
+   * @param  {Object} options.input - fieldProps.input
+   * @return {Function} decoratored onChange
+   */
+  handleFormFieldChange(
+    { input }:{input: {name: string, onChange: (evt:React.SyntheticEvent) => void}},
+   ):(evt:React.SyntheticEvent) => void {
+    return (evt) => {
+      const { formState } = this.props;
+      const { name, onChange } = input;
+      const { mutators: { setFieldTouched }} = formState;
+      setFieldTouched(name, false);
+      onChange(evt);
+    };
+  }
 
 
-export default FormInput;
+  /**
+   * Handles clearing of input value
+   * @param  {Object} options.input - fieldProps.input
+   * @param  {Object} inputRef - reference to the underlying input
+   * @return {Function} onClear handler
+   */
+  handleFieldClear(
+    { input }:{input: {name: string, onChange: (val:string) => void}},
+    inputRef?: {current: {focus: () => void}}
+    ){
+    return () => {
+      const { formState } = this.props;
+      const { name, onChange } = input;
+      const { mutators: { setFieldTouched }} = formState;
+      setFieldTouched(name, false);
+      onChange('');
+      if (inputRef && inputRef.current){
+        inputRef.current.focus();
+      }
+    };
+  }
+
+  /**
+   * Renders the base input field
+   * NOTE: It is required to pass a named function to component
+   * ... if you pass an anonymous function it will re-create the component
+   * @param  {[type]} fieldProps:any [description]
+   * @return {[type]}                [description]
+   */
+  renderFieldComponent(fieldProps:any){
+    const { forwardedRef } = this.props;
+    const { meta } = fieldProps;
+    return (
+      <BaseInput
+        {...fieldProps.input}
+        error={meta.touched && meta.error}
+        onChange={this.handleFormFieldChange(fieldProps)}
+        onClear={this.handleFieldClear(fieldProps, forwardedRef)}
+        forwardedRef={forwardedRef}
+        {...this.props} // Passed props take highest priority
+      />
+    );
+  }
+
+  render(){
+    const { name } = this.props;
+    return (
+      <Field
+        name={name}
+        component={this.renderFieldComponent}
+      />
+    );
+  }
+}
+
+
+export default withFormState(FormInput);
