12cb0682baae355fac31bf67806bdac30c1cc813
Eleazar Justo <EleazarJ@magenic.com>
2019-06-25 23:06:59 +0800

    dist file

--- dist/components/SelectListItem/SelectListItem.js
+++ dist/components/SelectListItem/SelectListItem.js
@@ -1,42 +1,37 @@
-import _defineProperty from '@babel/runtime/helpers/esm/defineProperty';
+import _defineProperty from "@babel/runtime/helpers/esm/defineProperty";
 import React from 'react';
 import invariant from 'tiny-invariant';
 import { withStyles } from '@material-ui/styles';
 import ListItem from '@material-ui/core/ListItem'; // Types
 
-const styleClasses = function styleClasses(theme) {
-  let _root;
+var styleClasses = function styleClasses(theme) {
+  var _root;
 
   return {
-    root:
-      ((_root = {
-        height: 48,
-        background: theme.secondaryPalette.colorVariables.TRANSPARENT,
-        fontSize: 18,
-        letterSpacing: 'normal',
-        fontStretch: 'normal',
-        lineHeight: 1.5,
-        fontFamily: theme.typography.fontFamily,
-      }),
-      _defineProperty(_root, theme.breakpoints.between('xs', 'sm'), {
-        fontSize: 16,
-      }),
-      _defineProperty(_root, '&.Mui-selected, &.Mui-selected:hover', {
-        backgroundColor: theme.secondaryPalette.colorVariables.SECONDARY_HOVER,
-      }),
-      _defineProperty(_root, '&:hover', {
-        backgroundColor: theme.secondaryPalette.colorVariables.SECONDARY_HOVER,
-      }),
-      _root),
+    root: (_root = {
+      height: 48,
+      background: theme.secondaryPalette.colorVariables.TRANSPARENT,
+      fontSize: 18,
+      letterSpacing: 'normal',
+      fontStretch: 'normal',
+      lineHeight: 1.5,
+      fontFamily: theme.typography.fontFamily
+    }, _defineProperty(_root, theme.breakpoints.between('xs', 'sm'), {
+      fontSize: 16
+    }), _defineProperty(_root, '&.Mui-selected, &.Mui-selected:hover', {
+      backgroundColor: theme.secondaryPalette.colorVariables.SECONDARY_HOVER
+    }), _defineProperty(_root, '&:hover', {
+      backgroundColor: theme.secondaryPalette.colorVariables.SECONDARY_HOVER
+    }), _root),
     gutters: {
-      padding: '0 13px 0 13px',
+      padding: '0 13px 0 13px'
     },
     divider: {
-      borderBottom: '1px solid '.concat(theme.palette.primary.main),
+      borderBottom: "1px solid ".concat(theme.palette.primary.main),
       '&:last-child': {
-        borderBottom: 'none',
-      },
-    },
+        borderBottom: 'none'
+      }
+    }
   };
 };
 
@@ -50,37 +45,33 @@ function checkValidity(item) {
   }
 }
 
-const SelectListItem = function SelectListItem(_ref) {
-  const { classes } = _ref;
-  const { item } = _ref;
-  const { onSelect } = _ref;
+var SelectListItem = function SelectListItem(_ref) {
+  var classes = _ref.classes,
+      item = _ref.item,
+      onSelect = _ref.onSelect;
   checkValidity(item);
-  const { display } = item;
-  const { id } = item;
-  const { value } = item;
-  const { divider } = classes;
-  const { gutters } = classes;
-  const { root } = classes;
-  return React.createElement(
-    ListItem,
-    {
-      'data-quid': 'SelectListItem-'.concat(id),
-      value,
-      classes: {
-        root,
-        divider,
-        gutters,
-      },
-      divider: true,
-      onClick: function onClick() {
-        return onSelect(item);
-      },
+  var display = item.display,
+      id = item.id,
+      value = item.value;
+  var divider = classes.divider,
+      gutters = classes.gutters,
+      root = classes.root;
+  return React.createElement(ListItem, {
+    "data-quid": "SelectListItem-".concat(id),
+    value: value,
+    classes: {
+      root: root,
+      divider: divider,
+      gutters: gutters
     },
-    display
-  );
+    divider: true,
+    onClick: function onClick() {
+      return onSelect(item);
+    }
+  }, display);
 };
 
 export default withStyles(styleClasses, {
   index: 0,
-  withTheme: true,
+  withTheme: true
 })(SelectListItem);
