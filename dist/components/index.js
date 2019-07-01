165539f7bb2e2232973a83aa844386724a4f62fb
Adam Schuld <elgrecode@gmail.com>
2019-07-01 12:11:24 -0700

    renames main index file to typescript and creates new build

--- dist/components/index.js
+++ dist/components/index.js
@@ -1,56 +1,182 @@
-// Main
-import AAAPrimaryTheme from './AAAPrimaryTheme/AAAPrimaryTheme';
-import Button from './Button/Button';
-import ButtonGroup from './ButtonGroup/ButtonGroup';
-import NumericInput from './Input/NumericInput/NumericInput';
-import BaseInput from './Input/BaseInput/BaseInput';
-import Label from './Label/Label';
-import Link from './Link/Link';
-import ToggleButtonGroup from './ToggleButtonGroup/ToggleButtonGroup';
-import RadioGroup from './RadioGroup/RadioGroup';
-import RadioItem from './RadioItem/RadioItem';
-import SelectList from './SelectList/SelectList';
-import SelectListItem from './SelectListItem/SelectListItem';
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(["exports", "./AAAPrimaryTheme/AAAPrimaryTheme", "./Button/Button", "./ButtonGroup/ButtonGroup", "./Input/NumericInput/NumericInput", "./Input/BaseInput/BaseInput", "./Label/Label", "./Link/Link", "./ToggleButtonGroup/ToggleButtonGroup", "./RadioGroup/RadioGroup", "./RadioItem/RadioItem", "./SelectList/SelectList", "./SelectListItem/SelectListItem", "./Body/Body", "./Headline/Headline", "./Subheadline/Subheadline", "./Subtitle/Subtitle", "./Form/Form", "./Form/FormGroup/FormGroup", "./Form/FormInput/FormInput", "./Form/FormNumericInput/FormNumericInput", "./Form/FormNumericalStepper/FormnumericalStepper", "../icons/RightArrowIcon", "../icons/AddIcon"], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports, require("./AAAPrimaryTheme/AAAPrimaryTheme"), require("./Button/Button"), require("./ButtonGroup/ButtonGroup"), require("./Input/NumericInput/NumericInput"), require("./Input/BaseInput/BaseInput"), require("./Label/Label"), require("./Link/Link"), require("./ToggleButtonGroup/ToggleButtonGroup"), require("./RadioGroup/RadioGroup"), require("./RadioItem/RadioItem"), require("./SelectList/SelectList"), require("./SelectListItem/SelectListItem"), require("./Body/Body"), require("./Headline/Headline"), require("./Subheadline/Subheadline"), require("./Subtitle/Subtitle"), require("./Form/Form"), require("./Form/FormGroup/FormGroup"), require("./Form/FormInput/FormInput"), require("./Form/FormNumericInput/FormNumericInput"), require("./Form/FormNumericalStepper/FormnumericalStepper"), require("../icons/RightArrowIcon"), require("../icons/AddIcon"));
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports, global.AAAPrimaryTheme, global.Button, global.ButtonGroup, global.NumericInput, global.BaseInput, global.Label, global.Link, global.ToggleButtonGroup, global.RadioGroup, global.RadioItem, global.SelectList, global.SelectListItem, global.Body, global.Headline, global.Subheadline, global.Subtitle, global.Form, global.FormGroup, global.FormInput, global.FormNumericInput, global.FormnumericalStepper, global.RightArrowIcon, global.AddIcon);
+    global.index = mod.exports;
+  }
+})(this, function (_exports, _AAAPrimaryTheme, _Button, _ButtonGroup, _NumericInput, _BaseInput, _Label, _Link, _ToggleButtonGroup, _RadioGroup, _RadioItem, _SelectList, _SelectListItem, _Body, _Headline, _Subheadline, _Subtitle, _Form, _FormGroup, _FormInput, _FormNumericInput, _FormnumericalStepper, _RightArrowIcon, _AddIcon) {
+  "use strict";
 
-// Typography
-import Body from './Body/Body';
-import Headline from './Headline/Headline';
-import Subheadline from './Subheadline/Subheadline';
-import Subtitle from './Subtitle/Subtitle';
+  var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-// Form Specific
-import Form from './Form/Form';
-import FormGroup from './Form/FormGroup/FormGroup';
-import FormInput from './Form/FormInput/FormInput';
-import FormNumericInput from './Form/FormNumericInput/FormNumericInput';
-import FormNumericalStepper from './Form/FormNumericalStepper/FormnumericalStepper';
-
-// Icons
-import RightArrowIcon from '../icons/RightArrowIcon';
-import AddIcon from '../icons/AddIcon';
-
-export {
-  AAAPrimaryTheme,
-  AddIcon,
-  BaseInput,
-  Body,
-  Button,
-  ButtonGroup,
-  Form,
-  FormGroup,
-  FormInput,
-  FormNumericInput,
-  FormNumericalStepper,
-  RadioGroup,
-  RadioItem,
-  Headline,
-  Label,
-  Link,
-  NumericInput,
-  RightArrowIcon,
-  SelectList,
-  SelectListItem,
-  Subheadline,
-  Subtitle,
-  ToggleButtonGroup,
-};
+  Object.defineProperty(_exports, "__esModule", {
+    value: true
+  });
+  Object.defineProperty(_exports, "AAAPrimaryTheme", {
+    enumerable: true,
+    get: function get() {
+      return _AAAPrimaryTheme.default;
+    }
+  });
+  Object.defineProperty(_exports, "Button", {
+    enumerable: true,
+    get: function get() {
+      return _Button.default;
+    }
+  });
+  Object.defineProperty(_exports, "ButtonGroup", {
+    enumerable: true,
+    get: function get() {
+      return _ButtonGroup.default;
+    }
+  });
+  Object.defineProperty(_exports, "NumericInput", {
+    enumerable: true,
+    get: function get() {
+      return _NumericInput.default;
+    }
+  });
+  Object.defineProperty(_exports, "BaseInput", {
+    enumerable: true,
+    get: function get() {
+      return _BaseInput.default;
+    }
+  });
+  Object.defineProperty(_exports, "Label", {
+    enumerable: true,
+    get: function get() {
+      return _Label.default;
+    }
+  });
+  Object.defineProperty(_exports, "Link", {
+    enumerable: true,
+    get: function get() {
+      return _Link.default;
+    }
+  });
+  Object.defineProperty(_exports, "ToggleButtonGroup", {
+    enumerable: true,
+    get: function get() {
+      return _ToggleButtonGroup.default;
+    }
+  });
+  Object.defineProperty(_exports, "RadioGroup", {
+    enumerable: true,
+    get: function get() {
+      return _RadioGroup.default;
+    }
+  });
+  Object.defineProperty(_exports, "RadioItem", {
+    enumerable: true,
+    get: function get() {
+      return _RadioItem.default;
+    }
+  });
+  Object.defineProperty(_exports, "SelectList", {
+    enumerable: true,
+    get: function get() {
+      return _SelectList.default;
+    }
+  });
+  Object.defineProperty(_exports, "SelectListItem", {
+    enumerable: true,
+    get: function get() {
+      return _SelectListItem.default;
+    }
+  });
+  Object.defineProperty(_exports, "Body", {
+    enumerable: true,
+    get: function get() {
+      return _Body.default;
+    }
+  });
+  Object.defineProperty(_exports, "Headline", {
+    enumerable: true,
+    get: function get() {
+      return _Headline.default;
+    }
+  });
+  Object.defineProperty(_exports, "Subheadline", {
+    enumerable: true,
+    get: function get() {
+      return _Subheadline.default;
+    }
+  });
+  Object.defineProperty(_exports, "Subtitle", {
+    enumerable: true,
+    get: function get() {
+      return _Subtitle.default;
+    }
+  });
+  Object.defineProperty(_exports, "Form", {
+    enumerable: true,
+    get: function get() {
+      return _Form.default;
+    }
+  });
+  Object.defineProperty(_exports, "FormGroup", {
+    enumerable: true,
+    get: function get() {
+      return _FormGroup.default;
+    }
+  });
+  Object.defineProperty(_exports, "FormInput", {
+    enumerable: true,
+    get: function get() {
+      return _FormInput.default;
+    }
+  });
+  Object.defineProperty(_exports, "FormNumericInput", {
+    enumerable: true,
+    get: function get() {
+      return _FormNumericInput.default;
+    }
+  });
+  Object.defineProperty(_exports, "FormNumericalStepper", {
+    enumerable: true,
+    get: function get() {
+      return _FormnumericalStepper.default;
+    }
+  });
+  Object.defineProperty(_exports, "RightArrowIcon", {
+    enumerable: true,
+    get: function get() {
+      return _RightArrowIcon.default;
+    }
+  });
+  Object.defineProperty(_exports, "AddIcon", {
+    enumerable: true,
+    get: function get() {
+      return _AddIcon.default;
+    }
+  });
+  _AAAPrimaryTheme = _interopRequireDefault(_AAAPrimaryTheme);
+  _Button = _interopRequireDefault(_Button);
+  _ButtonGroup = _interopRequireDefault(_ButtonGroup);
+  _NumericInput = _interopRequireDefault(_NumericInput);
+  _BaseInput = _interopRequireDefault(_BaseInput);
+  _Label = _interopRequireDefault(_Label);
+  _Link = _interopRequireDefault(_Link);
+  _ToggleButtonGroup = _interopRequireDefault(_ToggleButtonGroup);
+  _RadioGroup = _interopRequireDefault(_RadioGroup);
+  _RadioItem = _interopRequireDefault(_RadioItem);
+  _SelectList = _interopRequireDefault(_SelectList);
+  _SelectListItem = _interopRequireDefault(_SelectListItem);
+  _Body = _interopRequireDefault(_Body);
+  _Headline = _interopRequireDefault(_Headline);
+  _Subheadline = _interopRequireDefault(_Subheadline);
+  _Subtitle = _interopRequireDefault(_Subtitle);
+  _Form = _interopRequireDefault(_Form);
+  _FormGroup = _interopRequireDefault(_FormGroup);
+  _FormInput = _interopRequireDefault(_FormInput);
+  _FormNumericInput = _interopRequireDefault(_FormNumericInput);
+  _FormnumericalStepper = _interopRequireDefault(_FormnumericalStepper);
+  _RightArrowIcon = _interopRequireDefault(_RightArrowIcon);
+  _AddIcon = _interopRequireDefault(_AddIcon);
+});
